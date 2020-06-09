5963ae5411c9a5a901fb5759fe33ab12d8a6f3e0
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-09 23:39:29 +0530

    completed React App

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate'
+import CommentList from './CommentList';
 
 
 export default ()=>{
@@ -20,6 +21,7 @@ export default ()=>{
             key={post.id}>
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentList postId={post.id}/>
                     <CommentCreate postId={post.id}/>
                 </div>
             </div>
@@ -30,6 +32,9 @@ export default ()=>{
         <div className="d-flex flex-row flex-wrap justify-content-between">{renderedPosts}</div>
     )
 };
+
+
+
 
 
 
