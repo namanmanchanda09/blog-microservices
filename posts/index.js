352bc6b5a7744adcea2f972aee081d289333be75
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-10 15:28:02 +0530

    emitting Post Creation event

--- posts/index.js
+++ posts/index.js
@@ -16,14 +16,23 @@ app.get('/posts',(req,res)=>{
     res.send(posts);
 });
 
-app.post('/posts',(req,res)=>{
+app.post('/posts',async (req,res)=>{
     const id = randomBytes(4).toString('hex');
     const {title} = req.body;
     posts[id] = {id,title};
+    await axios.post('http://localhost:4005/events',{
+        type:'PostCreated',
+        data:{id,title,}
+    });
+
     res.status(201).send(posts[id]);
 });
 
 app.listen(4000,()=>{
     console.log(`Listening on 4000`);
 });
+
+
+
+
 
