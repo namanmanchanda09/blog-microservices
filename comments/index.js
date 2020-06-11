93e12e43193e5f433fe7eb9e68bb65f1386e5444
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-11 10:59:47 +0530

    completed Comment Moderation

--- comments/index.js
+++ comments/index.js
@@ -19,25 +19,48 @@ app.post('/posts/:id/comments',async (req,res)=>{
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({id:commentId, content});
+    comments.push({id:commentId, content,status:'pending'});
     commentsByPostId[req.params.id] = comments;
     await axios.post('http://localhost:4005/events',{
         type:'CommentCreated',
-        data:{id:commentId,content,postId:req.params.id}
+        data:{id:commentId,content,postId:req.params.id,status: 'pending'}
     })
     res.status(201).send(comments);
 
 });
 
-app.post('/events',(req,res)=>{
+app.post('/events',async (req,res)=>{
     console.log(`Received Event ${req.body}`);
-    console.log(`Type of event ${req.body.type}`)
+    console.log(`Type of event ${req.body.type}`);
+    const {type,data} = req.body;
+    if(type==='CommentModerated'){
+        const{postId, id, status,content} = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment=>{
+            return comment.id === id;
+        });
+        comment.status = status;
+        await axios.post('http://localhost:4005/events',{
+            type:'CommentUpdated',
+            data:{
+                id,
+                status,
+                postId,
+                content
+
+            }
+
+        })
+    }
     res.send({});
 });
 
 app.listen(4001,()=>{
     console.log(`Listening on 4001`);
 })
+
+
+
 
 
 
--- event-bus/index.js
+++ event-bus/index.js
@@ -8,16 +8,16 @@ app.use(bodyParser.json());
 app.post('/events',(req,res)=>{
     const event = req.body;
 
-    axios.post('http://localhost:4000/events',event);
-    axios.post('http://localhost:4001/events',event);
-    axios.post('http://localhost:4002/events',event);
+    axios.post('http://localhost:4000/events',event); // Post
+    axios.post('http://localhost:4001/events',event); // Comment
+    axios.post('http://localhost:4002/events',event); // Query
+    axios.post('http://localhost:4003/events',event); // Moderation
 
     res.send({status:'OK'});
 });
 
-
+// Event Bus Port 
 app.listen(4005,()=>{
     console.log(`Listening on 4005`);
 });
-
 
