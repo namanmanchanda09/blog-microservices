c7c6b6397063ac8c7aadfc22a96882637c44b0db
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-11 11:38:11 +0530

    changed the React App to show moderations

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -4,7 +4,17 @@ import React from 'react'
 export default ({comments})=>{
 
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}> {comment.content} </li>
+        let content;
+        if(comment.status==='approved'){
+            content = comment.content;
+        }
+        if(comment.status === 'pending'){
+            content = 'This comment is awaiting moderation';
+        }
+        if(comment.status === 'rejected'){
+            content = 'The comment has been rejected';
+        }
+        return <li key={comment.id}> {content} </li>
     });
 
     return (
@@ -14,6 +24,7 @@ export default ({comments})=>{
     );
 
 }
+
 
 
 
